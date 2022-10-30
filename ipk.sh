ab954822e30d83301b7b1dd7d0543c065ace5bb6
bianca2190 <109406227+bianca2190@users.noreply.github.com>
2022-10-30 14:44:30 +0700

    Update ipk.sh

--- ipk.sh
+++ ipk.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-pushd package/luci-app-openclash/tools/po2lmo
-make && sudo make install
-popd
-
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/sing-box package/sing-box
-svn co https://github.com/immortalwrt/homeproxy/trunk package/homeproxy
+git clone https://github.com/xiaorouji/openwrt-passwall.git
+git clone https://github.com/immortalwrt/homeproxy.git
+cp -rf openwrt-passwall/sing-box package/
+cp -rf homeproxy package/
+rm -rf openwrt-passwall/ homeproxy/
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
